4fe6bab59dc97bd6d769002b73f32cb854f8b100
Daniel <dmacrva@gmail.com>
2018-06-03 15:53:28 -0400

    hello world

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import path from 'path';
+
+const { NODE_ENV, PORT } = process.env;
+const app = express();
+
+app.set('port', PORT || 5000);
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(express.static(__dirname + '/../public'));
+
+console.log(__dirname + '/../public')
+
+app.get('/', (request, response) => (response.sendFile(path.join(__dirname, 'index.html'))));
+
+// app.use('*', (request, response) => (response.sendStatus(404)));
+
+app.listen(app.get('port'), () => {
+  console.log(`[${NODE_ENV}] listening on port ${app.get('port')}`);
+});
