31c1ad52679921b9965076bd219ec135d86c932d
Daniel <dmacrva@gmail.com>
2019-01-30 00:52:10 -0500

    added styles, fonts, some responsive stuff

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import './app.css';
+
 const App = () => (
-  <div className="App">
-    <h1>Hello world</h1>
+  <div className="container">
+    <h1 className="home-title pacifico">Daniel MacPhee</h1>
+    <hr className="home-title-divider" />
+    <p className="home-subtitle">A website with some stuff I've done</p>
+    <div className="list-of-things">
+      <a className="thing" href="#">Like this Breath Marches character generator, for instance</a>
+    </div>
+    <footer className="dark footer-text">© Daniel 2019</footer>
   </div>
 );
 
