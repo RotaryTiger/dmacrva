f9c2181b503af00c89216dca892301688e899380
Daniel <dmacrva@gmail.com>
2019-02-09 09:31:50 -0500

    fixed path to index.html

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
 
 app.use('/api', routes);
 
