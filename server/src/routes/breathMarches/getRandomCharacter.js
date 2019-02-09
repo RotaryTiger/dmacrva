8ab1f50a65ba1a6715b35f236e45bd87623edb32
Daniel <dmacrva@gmail.com>
2019-02-08 19:16:26 -0500

    added formatted json response query param

--- server/src/routes/breathMarches/getRandomCharacter.js
+++ server/src/routes/breathMarches/getRandomCharacter.js
@@ -2,6 +2,13 @@ import { randomCharacters } from '../../utils';
 
 const { createRandomCharacter } = randomCharacters;
 
-export default (request, response) => (
-  response.status(201).send(createRandomCharacter())
-);
+export default (request, response) => {
+  const { json } = request.query;
+  const character = createRandomCharacter();
+
+  if (json) {
+    return response.status(200).send(JSON.stringify(character, null, 4));
+  }
+
+  return response.status(200).send(character);
+};
